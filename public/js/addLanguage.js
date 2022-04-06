dca85132199f9b7e106965388b17d3ca70e791ad
Sudeep Rao <55048261+therealgeekfruit@users.noreply.github.com>
2022-04-06 11:11:06 +0530

    removed translations

--- public/js/addLanguage.js
+++ public/js/addLanguage.js
@@ -1,11 +1,11 @@
 //--------------TRANSLATIONS------------------
-fetch("/locale/getTranslations?page_id=9").then((res) => {
-    res.json().then((data) => {
-      document.querySelector("#uploadFile").innerHTML =
-        data.PageLabels.uploadFile;
-      document.querySelector("#logout").innerHTML = data.PageLabels.logout;
-      document.querySelector("#instruction").innerHTML = data.PageLabels.addLanguage;
-      document.querySelector("#addLanguageLabel").innerHTML = data.PageLabels.newLanguage;
-      document.querySelector("#submit").innerHTML = data.PageLabels.add;
-    });
-  });
+// fetch("/locale/getTranslations?page_id=9").then((res) => {
+//     res.json().then((data) => {
+//       document.querySelector("#uploadFile").innerHTML =
+//         data.PageLabels.uploadFile;
+//       document.querySelector("#logout").innerHTML = data.PageLabels.logout;
+//       document.querySelector("#instruction").innerHTML = data.PageLabels.addLanguage;
+//       document.querySelector("#addLanguageLabel").innerHTML = data.PageLabels.newLanguage;
+//       document.querySelector("#submit").innerHTML = data.PageLabels.add;
+//     });
+//   });
--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,16 @@
-fetch("http://localhost:3000/locale/getTranslations?page_id=1").then((res) => {
-  res.json().then((data) => {
-    console.log(data);
-    document.querySelector("#userDatabase").innerHTML =
-      data.PageLabels.userDatabase;
-    document.querySelector("#signUp").innerHTML = data.PageLabels.signUp;
-    document.querySelector("#userName").innerHTML = data.PageLabels.userName;
-    document.querySelector("#password").innerHTML = data.PageLabels.password;
-    document.querySelector("#submit").innerHTML = data.PageLabels.signIn;
-    document.querySelector("#signIn").innerHTML = data.PageLabels.signIn;
-    // if (data.PageLabels.signIns) {
-    //   document.querySelector("#signIn").innerHTML = data.PageLabels.signIns;
-    // } else {
-    //   document.querySelector("#signIn").innerHTML = "hello";
-    // }
-  });
-});
+// fetch("http://localhost:3000/locale/getTranslations?page_id=1").then((res) => {
+//   res.json().then((data) => {
+//     console.log(data);
+//     document.querySelector("#userDatabase").innerHTML =
+//       data.PageLabels.userDatabase;
+//     document.querySelector("#signUp").innerHTML = data.PageLabels.signUp;
+//     document.querySelector("#userName").innerHTML = data.PageLabels.userName;
+//     document.querySelector("#password").innerHTML = data.PageLabels.password;
+//     document.querySelector("#submit").innerHTML = data.PageLabels.signIn;
+//     document.querySelector("#signIn").innerHTML = data.PageLabels.signIn;
+
+//   });
+// });
 
 // let userDatabase = document.querySelector("userDatabase");
 // let signUp = document.querySelector("#signup");
