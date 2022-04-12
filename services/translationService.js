efb7c6aecdeb832a21f6fc61231683125ead4c5e
shank-codes <shashankhiremat22@gmail.com>
2022-04-12 11:36:40 +0530

    add translation DAO and service

--- services/translationService.js
+++ services/translationService.js
@@ -1,2 +1,20 @@
 const translationDAO = require("../DAO/translationDAO");
 const sequelize = require("../DAO/database");
+
+exports.saveTranslation = async (
+  labelId,
+  languageId,
+  transactionValue,
+  transaction
+) => {
+  try {
+    const saveResult = await translationDAO.saveTranslation(
+      { labelId, languageId, transactionValue },
+      transaction
+    );
+    return saveResult;
+  } catch (err) {
+    console.log(err);
+    return { Success: false, Error: err.message };
+  }
+};
